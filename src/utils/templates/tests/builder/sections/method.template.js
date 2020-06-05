a2670cbea6b7be6a88c7b2c7b75b841d4b605aa9
Hector Jimenez <hector.jimenez.sanchez@gmail.com>
2020-06-05 18:42:12 +0100

    #999 -fix(method.template.js) fix max length 140 characters issue

--- src/utils/templates/tests/builder/sections/method.template.js
+++ src/utils/templates/tests/builder/sections/method.template.js
@@ -3,5 +3,7 @@ import { replacements } from '../../../replacements';
 
 export function methodReq(method, path, idsFormatted) {
   const pathWithDummyData = buildUrl(path, idsFormatted);
-  return replacements().method.replace(/{method}/g, method).replace(/{path}/g, path.startsWith('/') ? pathWithDummyData : '/' + pathWithDummyData);
+  return replacements().method
+      .replace(/{method}/g, method)
+      .replace(/{path}/g, path.startsWith('/') ? pathWithDummyData : '/' + pathWithDummyData);
 }
