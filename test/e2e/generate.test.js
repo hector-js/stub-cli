86a3fa91fda28654a589811b14bb4b7912e76ab5
Hector Jimenez <hector.jimenez.sanchez@gmail.com>
2022-03-04 23:32:18 +0000

    #26: feat(generate.test.js) add edge cases

--- test/e2e/generate.test.js
+++ test/e2e/generate.test.js
@@ -6,53 +6,121 @@ const fs = require('fs');
 
 describe('generate', () => {
   describe('get method', ()=>{
-    const pathResource = '_hjs/resources/my-path-get.json';
-    const pathTest = '_hjs/test/my-path-get.test.js';
-
     before(()=>{
       cd('example');
-      exec('npm run hjs -- generate get my/path');
     });
 
     after(()=>{
       cd('..');
     });
 
-    describe('resource', ()=>{
-      it('creates the resource file', () => {
-        expect(fs.existsSync(pathResource)).to.be.true;
-      }).timeout(200000);
+    describe('basic', ()=>{
+      const pathResource = '_hjs/resources/my-path-get.json';
+      const pathTest = '_hjs/test/my-path-get.test.js';
 
-      it('contains get method', ()=>{
-        const response = JSON.parse(fs.readFileSync(pathResource).toString());
+      describe('resource', ()=>{
+        it('contains get method', ()=>{
+          exec('npm run hjs -- generate get my/path');
 
-        expect(response).to.deep.eq({
-          _get: {
-            '/my/path': [
-              {
-                _req: {},
-                _res: {
-                  _body: {
-                    body: 'To be defined'
-                  }
-                },
-                _description: 'Description to be defined'
-              }
-            ]
-          }
-        });
+          expect(fs.existsSync(pathResource)).to.be.true;
+          const response = JSON.parse(fs.readFileSync(pathResource).toString());
+          expect(response).to.deep.eq({
+            _get: {
+              '/my/path': [
+                {
+                  _req: {},
+                  _res: {
+                    _body: {
+                      body: 'To be defined'
+                    }
+                  },
+                  _description: 'Description to be defined'
+                }
+              ]
+            }
+          });
+        }).timeout(200000);
+      });
+
+      describe('test', ()=>{
+        it('contains test method', ()=>{
+          exec('npm run hjs -- generate get my/path');
+
+          expect(fs.existsSync(pathTest)).to.be.true;
+          const actual = fs.readFileSync(pathTest).toString();
+
+          expect(actual).to.eq(basicDataGet);
+        }).timeout(200000);
       });
     });
 
-    describe('test', ()=>{
-      it('creates the test file', () => {
-        expect(fs.existsSync(pathTest)).to.be.true;
-      }).timeout(200000);
+    describe('edge cases', ()=>{
+      const pathResource = '_hjs/resources/my-package/my-path-get.json';
+      const pathTest = '_hjs/test/my-package/my-path-get.test.js';
 
-      it('contains test method', ()=>{
-        const response = fs.readFileSync(pathTest).toString();
+      describe('resource', ()=>{
+        it('contains get method', ()=>{
+          exec('npm run hjs --'+
+          ' generate get my/path '+
+          '--package my-package '+
+          '--description "My description for this method" '+
+          '--headers "authorization,clientId" '+
+          '--cookies "JSSESSION,TS123456" '+
+          '--delay 1000 '+
+          '--status 202');
 
-        expect(response).to.eq(`'use strict';
+          expect(fs.existsSync(pathResource)).to.be.true;
+          const response = JSON.parse(fs.readFileSync(pathResource).toString());
+          expect(response).to.deep.eq({
+            '_get': {
+              '/my/path': [
+                {
+                  '_req': {
+                    '_headers': [
+                      'authorization',
+                      'clientId'
+                    ],
+                    '_cookies': [
+                      'JSSESSION',
+                      'TS123456'
+                    ]
+                  },
+                  '_res': {
+                    '_status': 202,
+                    '_body': {
+                      'body': 'To be defined'
+                    },
+                    '_delay': 1000
+                  },
+                  '_description': 'My description for this method'
+                }
+              ]
+            }
+          });
+        }).timeout(200000);
+      });
+
+      describe('test', ()=>{
+        it('contains test method', ()=>{
+          exec('npm run hjs --'+
+          ' generate get my/path '+
+          '--package my-package '+
+          '--description "My description for this method" '+
+          '--headers "authorization,clientId" '+
+          '--cookies "JSSESSION,TS123456" '+
+          '--delay 1000 '+
+          '--status 202');
+
+          expect(fs.existsSync(pathTest)).to.be.true;
+          const response = fs.readFileSync(pathTest).toString();
+          expect(response).to.eq(edgeCaseGet);
+        }).timeout(200000);
+      });
+    });
+  });
+});
+
+const basicDataGet = `'use strict';
     
 var app = require('@hectorjs/stub-backend');
 var chai = require('chai');
@@ -73,8 +141,29 @@ describe('GET - my/path ', () => {
           done();
       });
   });
-});`);
+});`;
+
+const edgeCaseGet = `'use strict';
+    
+var app = require('@hectorjs/stub-backend');
+var chai = require('chai');
+var request = require('supertest');
+    
+var expect = chai.expect;
+
+describe('GET - my/path ', () => {
+  it('should exist', (done) => {
+    request(app)
+      .get('/my/path')
+      .set({"authorization": "any value","clientId": "any value"})
+      .set('Cookie', ["JSSESSION=anyValue","TS123456=anyValue"])
+      .end((err, res) => {
+          expect(err).to.not.exist;
+          expect(res.status).to.equal(202);
+          expect(res.body).to.deep.equal({
+            'body': 'To be defined'
+          });
+          done();
       });
-    });
-  });
-});
+  }).timeout(1500);
+});`;
