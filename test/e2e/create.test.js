cfefb7278124bf353ef54f8d7e891c8d63622b81
Hector Jimenez <hector.jimenez.sanchez@gmail.com>
2022-10-16 17:30:36 +0100

    update to version 1.35.0 stub-backend;

--- test/e2e/create.test.js
+++ test/e2e/create.test.js
@@ -100,7 +100,7 @@ describe('new', () => {
       author: '',
       license: 'ISC',
       dependencies: {
-        '@hectorjs/stub-backend': '^1.34.0'
+        '@hectorjs/stub-backend': '^1.35.0'
       }
     });
   });
